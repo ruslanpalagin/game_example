353ef13b69b4245e18a8a6c4680652abc9584a70
Ruslan Palagin <ruslan.palagin.ck@gmail.com>
2019-06-21 22:24:10 +0300

    #master: add pixi

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import PIXI from "./vendor/PIXI";
+import charUrl from "./textures/char.png";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+    componentDidMount = () => {
+        const app = new PIXI.Application({
+            antialias: true,    // default: false
+            transparent: false, // default: false
+            resolution: 1       // default: 1
+        });
+        app.renderer.backgroundColor = 0xeeeeee;
+        app.renderer.resize(window.innerWidth - 50, window.innerHeight - 50 );
+        document.getElementById("game__main-frame").appendChild(app.view);
+
+        PIXI.loader = new PIXI.Loader();
+        PIXI.loader
+        .add("/textures/char.png")
+        .load(() => {
+            console.log("charUrl", charUrl);
+            console.log("sss",  PIXI.loader.resources[charUrl].texture);
+            let cat = new PIXI.Sprite(PIXI.loader.resources[charUrl].texture);
+
+            //Add the cat to the stage
+            app.stage.addChild(cat);
+        });
+
+
+    }
+
+    render = () => {
+      return (
+          <div className="">
+            <div className="game__main-frame" id="game__main-frame" />
+          </div>
+      );
+  };
 }
 
 export default App;
---
+++ src/vendor/PIXI.js
@@ -0,0 +1,3 @@
+const PIXI = require("pixi.js");
+
+export default PIXI;
