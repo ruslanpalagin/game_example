5b57e04d8f13f7d1474bba20f50aa53208839844
Ruslan Palagin <ruslan.palagin.ck@gmail.com>
2019-06-21 22:30:50 +0300

    #master: eject react

--- src/App.js
+++ src/App.js
@@ -2,32 +2,35 @@ import React from 'react';
 import './App.css';
 import PIXI from "./vendor/PIXI";
 import charUrl from "./textures/char.png";
+import mountainsUrl from "./textures/mountains.png";
+import treesUrl from "./textures/trees.png";
 
 class App extends React.Component {
 
     componentDidMount = () => {
         const app = new PIXI.Application({
             antialias: true,    // default: false
-            transparent: false, // default: false
+            transparent: true, // default: false
             resolution: 1       // default: 1
         });
-        app.renderer.backgroundColor = 0xeeeeee;
         app.renderer.resize(window.innerWidth - 50, window.innerHeight - 50 );
         document.getElementById("game__main-frame").appendChild(app.view);
 
         PIXI.loader = new PIXI.Loader();
         PIXI.loader
-        .add("/textures/char.png")
+        .add(charUrl)
+        .add(mountainsUrl)
+        .add(treesUrl)
         .load(() => {
-            console.log("charUrl", charUrl);
-            console.log("sss",  PIXI.loader.resources[charUrl].texture);
             let cat = new PIXI.Sprite(PIXI.loader.resources[charUrl].texture);
-
-            //Add the cat to the stage
             app.stage.addChild(cat);
+            let m = new PIXI.Sprite(PIXI.loader.resources[mountainsUrl].texture);
+            m.position.y = 100;
+            app.stage.addChild(m);
+            let t = new PIXI.Sprite(PIXI.loader.resources[treesUrl].texture);
+            t.position.x = 100;
+            app.stage.addChild(t);
         });
-
-
     }
 
     render = () => {
