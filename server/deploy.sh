bfa311d12b8bbd90b92351e6c58f958614e5d214
Ruslan Palagin <ruslan.palagin.ck@gmail.com>
2019-08-09 22:26:21 +0300

    [S] #view_target_refactor: fix minor server deployment bug

--- server/deploy.sh
+++ server/deploy.sh
@@ -1,4 +1,4 @@
-ssh dev@35.240.39.143 "cd ~/production/game_example && git add -A && git reset --hard && git pull && git checkout $1"
+ssh dev@35.240.39.143 "cd ~/production/game_example && git add -A && git reset --hard && git checkout master && git pull && git checkout $1"
 
 ssh dev@35.240.39.143 "mkdir -p ~/production/game_example/server/bin/remote"
 scp ./bin/remote/npm_i.sh dev@35.240.39.143:~/production/game_example/server/bin/remote/npm_i.sh
