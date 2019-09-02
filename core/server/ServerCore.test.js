ea1376f8844873332d7a71956af8473062acbd77
Ruslan Palagin <ruslan.palagin.ck@gmail.com>
2019-09-02 11:40:28 +0300

    #beh_manager: move WorldStateMock

--- core/server/ServerCore.test.js
+++ core/server/ServerCore.test.js
@@ -1,22 +1,7 @@
 const ServerCore = require("./ServerCore");
-const WorldState = require("../state/WorldState");
-const CharFactory = require("../state/CharFactory");
+const WorldStateMock = require("../tests/WorldStateMock");
 const expect = require("expect");
 
-class WorldStateMock extends WorldState {
-    loadSave() {
-        const realmUnits = [
-            CharFactory.initEmptyCharacter({
-                wishes: [
-                    { name: "FollowWish", targetUnitId: this.uniqueId() },
-                ],
-            }),
-        ];
-        this.state.units = realmUnits;
-
-        return Promise.resolve();
-    }
-}
 
 describe("ServerCore", () => {
     describe("with wishManager", () => {
